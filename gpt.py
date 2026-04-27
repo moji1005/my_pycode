fd33b9cf7bbbdecaffdbdf440637ac71b2129cc7
mojtabaRaoufi <133561124+moji1005@users.noreply.github.com>
2026-04-27 12:51:23 -0700

    Update gpt.py

--- gpt.py
+++ gpt.py
@@ -1,15 +1,9 @@
 from g4f.client import Client
-from balethon import Client
-import os
-bot = Clinet(os.getenv("BALE_TOKEN"))
-@bot.on_message()
-async def botgetmesage(piam):
+
 client = Client()
 response = client.chat.completions.create(
     model="gpt-4.1",  # Try "gpt-4o", "deepseek-v3", etc.
-    messages=[{"role": "user", "content": piam}],
+    messages=[{"role": "user", "content": "Hello"}],
     web_search=False
 )
-await piam.reply(response.choices[0].message.content)
-
-bot.run()
+print(response.choices[0].message.content)
